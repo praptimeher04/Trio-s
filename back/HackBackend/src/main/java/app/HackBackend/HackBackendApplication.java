a8de41d35cee343444f3f225c5f17c99eb5bdf73
praptimeher04 <praptimeher04@gmail.com>
2026-09-05 14:58:14 +0530

    first commit

---
+++ back/HackBackend/src/main/java/app/HackBackend/HackBackendApplication.java
@@ -0,0 +1,13 @@
+package app.HackBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackBackendApplication.class, args);
+	}
+
+}
---
+++ back/HackBackend/src/test/java/app/HackBackend/HackBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package app.HackBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
